017459d1df39959fd1cf500fc66e571b2a8548c1
Miriam Cristina Zarzu <145344909+MiriamCristinaZ@users.noreply.github.com>
2023-12-28 17:53:35 +0000

    get the selected item and function to populate options in the selected element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 //Muscle groups and save choice into the local storage for the modal
 // Array of muscle groups
 const muscleGroupsArray = ["Chest", "Back", "Legs", "Arms", "Shoulders"];
+// Get the selected element
+const muscleGroupsSelect = document.getElementById("muscleGroups");
+
+// Function to populate options in the select element
+function populateMuscleGroups() {
+  muscleGroupsArray.forEach((muscleGroup) => {
+    const option = document.createElement("option");
+    option.value = muscleGroup;
+    option.text = muscleGroup;
+    muscleGroupsSelect.appendChild(option);
+  });
+}
