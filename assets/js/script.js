0aae965b348eee1e22d2c806fa52ff13088e4408
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2023-12-29 20:45:41 +0000

    added helpful comments, reorganized code logically and removed leftover code from previous merges

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,3 @@
-//Muscle groups and save choice into the local storage for the modal
-// Array of muscle groups
-const muscleGroupsArray = [
-                    "Abdominals",
-                    "Abductors",
-                    "Biceps",
-                    "Calves",
-                    "Chest",
-                    "Forearms",
-                    "Glutes",
-                    "Hamstrings",
-                    "Lats",
-                    "Lower Back",
-                    "Middle Back",
-                    "Neck",
-                    "Quadrceps",
-                    "Traps",
-                    "Triceps"
-];
-// Get the selected element
-const muscleGroupsSelect = document.getElementById("muscleGroups");
-
-// Function to populate options in the select element
-function populateMuscleGroups() {
-  muscleGroupsArray.forEach((muscleGroup) => {
-    const option = document.createElement("option");
-    option.value = muscleGroup;
-    option.text = muscleGroup;
-    muscleGroupsSelect.appendChild(option);
-  });
-}
-// Initial population of options
-populateMuscleGroups();
-
-// Event listener for the change event on the select element
-muscleGroupsSelect.addEventListener("change", function () {
-  // Get the selected value
-  const selectedMuscleGroup = muscleGroupsSelect.value;
-
-  // Store the selected value in local storage
-  localStorage.setItem("selectedMuscleGroup", selectedMuscleGroup);
-});
-
 const table = $("tbody");
 
 const displayTable = () => {
@@ -82,38 +39,84 @@ const displayTable = () => {
   }
 };
 
-// Time increment/decrement function:
+displayTable();
+
+//Muscle groups and save choice into the local storage for the modal
+// Array of muscle groups
+const muscleGroupsArray = [
+  "Abdominals",
+  "Abductors",
+  "Biceps",
+  "Calves",
+  "Chest",
+  "Forearms",
+  "Glutes",
+  "Hamstrings",
+  "Lats",
+  "Lower Back",
+  "Middle Back",
+  "Neck",
+  "Quadrceps",
+  "Traps",
+  "Triceps",
+];
+// Get the selected element
+const muscleGroupsSelect = document.getElementById("muscleGroups");
+
+// Function to populate options in the select element
+function populateMuscleGroups() {
+  muscleGroupsArray.forEach((muscleGroup) => {
+    const option = document.createElement("option");
+    option.value = muscleGroup;
+    option.text = muscleGroup;
+    muscleGroupsSelect.appendChild(option);
+  });
+}
+// Initial population of options
+populateMuscleGroups();
+
+// Event listener for the change event on the select element
+muscleGroupsSelect.addEventListener("change", function () {
+  // Get the selected value
+  const selectedMuscleGroup = muscleGroupsSelect.value;
+
+  // Store the selected value in local storage
+  localStorage.setItem("selectedMuscleGroup", selectedMuscleGroup);
+});
+
+// Time increment/decrement function
 $(document).ready(function () {
   // Initializing DateTimePicker
-  $('#time').datetimepicker({
-   format: 'HH:mm',
-   stepping: 15
+  $("#time").datetimepicker({
+    format: "HH:mm",
+    stepping: 15,
   });
- 
+
   // default time is 12:00
-  $('#time').val('12:00');
- 
+  $("#time").val("12:00");
+
   // Function to increment time by 15 minutes
   function incrementTime() {
-   var select = $("#time");
-   var currentTime = select.val();
-   var newTime = moment(currentTime, "HH:mm").add(15, "minute").format("HH:mm");
-   select.val(newTime);
-   select.trigger('change'); 
+    let select = $("#time");
+    let currentTime = select.val();
+    let newTime = moment(currentTime, "HH:mm")
+      .add(15, "minute")
+      .format("HH:mm");
+    select.val(newTime);
+    select.trigger("change");
   }
- 
+
   // Function to decrement time by 15 minutes
   function decrementTime() {
-   var select = $("#time");
-   var currentTime = select.val();
-   var newTime = moment(currentTime, "HH:mm").subtract(15, "minute").format("HH:mm");
-   select.val(newTime);
-   select.trigger('change');
+    let select = $("#time");
+    let currentTime = select.val();
+    let newTime = moment(currentTime, "HH:mm")
+      .subtract(15, "minute")
+      .format("HH:mm");
+    select.val(newTime);
+    select.trigger("change");
   }
- 
+
   $("#incrementBtn").click(incrementTime);
   $("#decrementBtn").click(decrementTime);
- });
- 
-
-displayTable();
+});
